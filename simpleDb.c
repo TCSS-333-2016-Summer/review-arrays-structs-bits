d3545bcf872aa2438a9f4601685ae1fe591278ab
Git-Admin <lhhunghimself@gmail.com>
2016-08-08 09:16:23 -0700

    Part4 only

--- simpleDb.c
+++ simpleDb.c
@@ -8,71 +8,9 @@ typedef struct {
 	int age;
 }clientRecord;
 
-void clientRecord_init_data(clientRecord *c, char *name, char gender, int age);
-void clientRecord_destroy(clientRecord *c);
-
-int clientRecord_read_text(char *fileName, clientRecord **array);
-int clientRecord_read_text_realloc(char *fileName, clientRecord **array);
-void clientRecord_write_text(char *fileName, clientRecord *array,int nRecords);
-
 void clientRecord_sort(clientRecord *c,int nRecords);
 int clientRecord_cmp(const void *a, const void *b);
 
-void clientRecord_init_data(clientRecord *c, char *name, char gender, int age){
-	c->name=malloc(strlen(name)+1);
-	strcpy(c->name,name);
-	c->age=age;
-	c->gender=gender;
-}
-void clientRecord_destroy(clientRecord *c){
-	if(c->name){ 
-		free (c->name);
-		c->name=0;
-	}
-	c->age=0;
-	c->gender=0;
-}
-
-int clientRecord_read_text(char *fileName, clientRecord **array){
-	FILE *fp=fopen(fileName,"r");
- clientRecord *myArray= (clientRecord *) malloc(sizeof(clientRecord));
- int maxElements=1;
- char line[512];
- int nLines=0;
-	while(fgets(line,sizeof(line),fp)){
-		while(nLines >= maxElements){
-			maxElements *=2;
-			myArray=realloc(myArray,maxElements*sizeof(clientRecord));
-		}	
-		int age;
-		char gender;
-		char name[512];
-		sscanf(line,"%s %c %d",name,&gender,&age);
-		fprintf(stderr,"%s %c %d\n",name,gender,age);
-		clientRecord_init_data(myArray+nLines,name,gender,age);
-		nLines++;
-	}
-	fclose(fp);
-	*array=myArray;
-	return nLines;
-}
-	
-void clientRecord_write_text(char *fileName, clientRecord *array,int nRecords){
-	FILE *fp=fopen(fileName,"w");
-	for(int i=0;i<nRecords;i++){
-	 fprintf(fp,"%s %c %d\n",array[i].name,array[i].gender,array[i].age);
-	}
-	fclose(fp);
-}	
-	void clientRecord_sort(clientRecord *c,int nRecords);
-int clientRecord_cmp(const void *a, const void *b);
-
-int clientRecord_cmp(const void *a, const void *b){
-	clientRecord *ca = (clientRecord*)a;
-	clientRecord *cb = (clientRecord*)b;
-	if(ca->age == cb->age)return strcmp(ca->name,cb->name);
-	return ca->age -cb->age;
-}	 
 void clientRecord_sort(clientRecord *c,int nRecords);
 int clientRecord_cmp(const void *a, const void *b);
 
